24a4cb52b9ab2c5a8404999ec2a35c7e2ea4efd0
Byorun <byorun@cyber-cortex.net>
2018-06-24 11:40:16 +0200

    renamed logos

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
           crossorigin="anonymous">
 
-    <!-- For Dev information, please see the wiki https://wiki.beatsaver.com/index.php?title=BeatSaver -->
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
@@ -29,7 +28,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.php"><img src="{{ asset('/img/Beat_Saver_Logo_White.png') }}" height="35em" style="margin-top: -7px;"></a>
+            <a class="navbar-brand" href="index.php"><img src="{{ asset('/img/beat_saver_logo_white.png') }}" height="35em" style="margin-top: -7px;"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
@@ -56,12 +55,6 @@
 <div class="container">
     <div class="row">
         <div class="col-md-1"><br><br><br><br></div>
-        <div class="col-md-12">
-            {{--<div class="alert alert-info" role="alert"> Looking for devlopers to help improve BeatSaver! Check out the <a href=https://github.com/beatsaver/beatsaver>Github repo</a>. Complete Rewrites--}}
-            {{--are welcome! Chat with me on the <a href=https://discord.gg/ZY8T8ky>Discord</a>, I'm Balsa.--}}
-            {{--</div>--}}
-            {{--</div>--}}
-        </div>
     </div>
     <div class="row">
         @yield('content')
@@ -73,20 +66,6 @@
                         href="https://github.com/beatsaver/beatsaver">Github</a></b>
         </p></center>
 </footer>
-{{--<script type="text/javascript">--}}
-{{--var _paq = _paq || [];--}}
-{{--_paq.push(['trackPageView']);--}}
-{{--_paq.push(['enableLinkTracking']);--}}
-{{--_paq.push(['setRequestMethod', 'POST']);--}}
-{{--_paq.push(["setRequestMethod", "POST"]);--}}
-{{--(function() {--}}
-{{--var u="//beatsaver.com/track/";--}}
-{{--_paq.push(['setRequestMethod', 'POST']);--}}
-{{--_paq.push(['setTrackerUrl', u+'console.php']);--}}
-{{--_paq.push(['setSiteId', '1']);--}}
-{{--var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];--}}
-{{--g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'console.js'; s.parentNode.insertBefore(g,s);--}}
-{{--})();--}}
-{{--</script>--}}
+
 </body>
 </html>

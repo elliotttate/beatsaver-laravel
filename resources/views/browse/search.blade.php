026d319183b7b13b2fc1e19d503e05714863328f
Byorun <byorun@cyber-cortex.net>
2018-07-05 19:46:25 +0200

    restored accidentally deleted view

--- resources/views/browse/search.blade.php
+++ resources/views/browse/search.blade.php
@@ -1,14 +1,17 @@
 @extends('layout')
-@section('title', '- Search')
+@section('title', '- Search results')
 
 @section('content')
+
     <div class="container">
 
         <form class="form-signin" method="post" action="{{ route('search.submit',['type' => 'all']) }}">
             {{ csrf_field() }}
-            <input type="text" id="inputSearchKey" class="form-control" placeholder="Name / Song Name / Author" name="key" minlength="3" required autofocus>
+            <input type="text" id="inputSearchKey" class="form-control" placeholder="Name / Song Name / Author" name="key" minlength="3" value="{{ $key }}" required autofocus>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Search</button>
             <br>
         </form>
     </div>
+
+    @each('browse.song-data-frontpage',$songs,'song')
 @endsection

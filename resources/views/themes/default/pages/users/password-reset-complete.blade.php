4ff1442d5112d7a0e12efed134f034474d19d64e
Jack Baron <jackmbaron@gmail.com>
2018-09-23 14:43:35 +0100

    Convert password reset

--- resources/views/themes/default/pages/users/password-reset-complete.blade.php
+++ resources/views/themes/default/pages/users/password-reset-complete.blade.php
@@ -2,16 +2,47 @@
 @section('title', '- Complete Password Reset')
 
 @section('content')
-    <div class="container">
-        <form class="form-signin" method="post" action="{{ route('password.reset.complete.submit') }}">
-            {{ csrf_field() }}
-            <h2 class="form-signin-heading">Forgot Password</h2><br>Don't forget to check your spam!<br>
-            <label for="inputEmail" class="sr-only">EMail Address</label>
-            <input type="hidden" name="token" value="{{ $token }}">
-            <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email Address" required autofocus>
-            <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-            <input type="password" name="password_confirmation" id="inputPasswordConfirm" placeholder="Password Confirm" class="form-control"  required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Reset</button>
-        </form>
+    <div class="content">
+        <div style="display: flex;flex-direction: column;align-items: center;">
+            <form style="max-width: 600px;width: 100%;" method="post" action="{{ route('password.reset.complete.submit') }}">
+                <h2>Forgot Password</h2>
+                <h5>Reset your password.</h5>
+
+                {{ csrf_field() }}
+                <input type="hidden" name="token" value="{{ $token }}">
+
+                <div class="field">
+                    <label class="label">Email Address</label>
+                    <div class="control has-icons-left">
+                        <input type="email" name="email" id="inputEmail" class="input" placeholder="Email Address" required autofocus>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control has-icons-left">
+                        <input type="password" name="password" id="inputPassword" class="input" placeholder="Password" required>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-lock"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Password Confirm</label>
+                    <div class="control has-icons-left">
+                        <input type="password" name="password_confirmation" id="inputPasswordConfirm" placeholder="Password Confirm" class="input" required>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-lock"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <button class="button is-link is-fullwidth" type="submit">Reset</button>
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/themes/default/pages/users/password-reset.blade.php
+++ resources/views/themes/default/pages/users/password-reset.blade.php
@@ -2,13 +2,26 @@
 @section('title', '- Reset Password')
 
 @section('content')
-    <div class="container">
-        <form class="form-signin" method="post" action="{{ route('password.reset.request.submit') }}">
-            {{ csrf_field() }}
-            <h2 class="form-signin-heading">Forgot Password</h2><br>Don't forget to check your spam!<br>
-            <label for="inputEmail" class="sr-only">EMail Address</label>
-            <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email Address" required autofocus>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Send Email</button>
-        </form>
+    <div class="content">
+        <div style="display: flex;flex-direction: column;align-items: center;">
+            <form style="max-width: 600px;width: 100%;" method="post" action="{{ route('password.reset.request.submit') }}">
+                <h2>Forgot Password</h2>
+                <h5>Don't forget to check your spam!</h5>
+                {{ csrf_field() }}
+
+                <div class="field has-addons">
+                    <div class="control is-expanded has-icons-left">
+                        <input type="email" name="email" id="inputEmail" class="input" placeholder="Email Address" required autofocus>
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-envelope"></i>
+                        </span>
+                    </div>
+
+                    <div class="control">
+                        <button class="button is-link" type="submit">Send Email</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection

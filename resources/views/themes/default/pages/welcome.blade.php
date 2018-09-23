711288c02f78f6af5afe642c363c89e604803718
Jack Baron <jackmbaron@gmail.com>
2018-09-23 14:44:36 +0100

    Fix this class

--- resources/views/themes/default/pages/welcome.blade.php
+++ resources/views/themes/default/pages/welcome.blade.php
@@ -2,5 +2,5 @@
 @section('title', '- Home')
 
 @section('content')
-    <h1 class="text-center">Welcome to BeatSaver, the unofficial custom song download platform.</h1>
+    <h1 class="has-text-centered">Welcome to BeatSaver, the unofficial custom song download platform.</h1>
 @endsection

36465192ef0ccfde4b97f028f351b5a825621e35
Jack Baron <jackmbaron@gmail.com>
2018-09-23 14:07:51 +0100

    Song preview and detail upgrades

--- resources/views/themes/default/components/song-detail.blade.php
+++ resources/views/themes/default/components/song-detail.blade.php
@@ -1,70 +1,77 @@
-<h2>{{ $name }}</h2>
-<table id="song-{{ $id }}" class="table" style="table-layout:fixed;">
-    <tr>
-        <th rowspan="6" style="width: 15%;" class="text-center">
-            <div>
-                <img src="{{ $coverUrl }}" alt="{{ $name }}" style="min-width: 10em; max-width: 10em;">
-            </div>
-            <br/>
-            <div>
-                <a class="btn btn-default" href="{{ $downloadUrl }}" role="button">Download File</a>
-            </div>
-            <br/>
-            <div>
-                <a class="btn btn-default" href="https://bsaber.com/songs/{{ $id }}" role="button">view at bsaber.com</a>
-            </div>
-            <br/>
-            <div>
-                <a class="btn btn-default" href="{{ route('browse.top.newest') }}">Back</a>
-            </div>
-        </th>
-        <th colspan="2">
-            <small>Uploaded by: <a href="{{ route('browse.user',['id' => $uploaderId]) }}">{{ $uploader }}</a> ({{ $createdAt }})</small>
-        </th>
-    </tr>
-    <tr>
-        <td>Song: {{ $songName }} - {{ $songSubName }}</td>
-        <td class="text-right">Version: {{$version}}</td>
-    </tr>
-    <tr>
-        <td>Author: {{ $authorName }}</td>
-        <td>Difficulties: {{ $difficulties }}</td>
-    </tr>
-    <tr>
-        <td>
-            Downloads: {{ $downloadCount }} || Finished: {{ $playedCount }}
-        </td>
-        <td>
-            Lighting Events: {{ $events }}
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2">{!! nl2br(e($description)) !!}</td>
-    </tr>
-    <tr>
-        <td>
-            @auth
-                <div>
-                    <form action="{{ route('votes.submit',['key' => $key]) }}" method="post">
-                        {{ csrf_field() }}
-                        <button type="submit" class="btn btn-default" name="type" value="up">
-                            <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> Up {{ $upVotes }}
-                        </button>
-                        <button type="submit" class="btn btn-default" name="type" value="down">
-                            <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> Down {{ $downVotes }}
-                        </button>
-                    </form>
-                </div>
-                <br/>
-            @endauth
-        </td>
-        <td class="text-right">
-            @auth
-                @if(auth()->id() == $uploaderId)
-                    <a class="btn btn-default btn-primary" href="{{ route('browse.detail.edit',['id' => $id]) }}" role="button">Edit</a>
-                    <a class="btn btn-default btn-danger" href="{{ route('browse.detail.delete',['id' => $id]) }}" role="button">Delete</a>
-                @endif
-            @endauth
-        </td>
-    </tr>
-</table>
+<div>
+    <h2 style="border-bottom: 1px solid #dbdbdb;margin-bottom: 10px;">
+        <span class="has-text-weight-semibold is-size-3">{{ $name }}</span>
+    </h2>
+
+    <div class="columns is-mobile">
+        <div class="column" style="max-width: 200px;">
+            <img src="{{ $coverUrl }}" alt="{{ $name }}" class="image" style="border-radius: 8px;">
+
+            <a class="button is-fullwidth has-text-weight-bold" href="{{ $downloadUrl }}" style="margin-top: 8px;">Download File</a>
+            <a class="button is-fullwidth has-text-weight-bold" href="https://bsaber.com/songs/{{ $id }}" style="margin-top: 8px;">View on BeastSaber</a>
+            <a class="button is-fullwidth has-text-weight-bold" href="{{ route('browse.top.newest') }}" style="margin-top: 8px;">Back</a>
+        </div>
+
+        <div class="column">
+            <table class="table is-fullwidth">
+                <tr>
+                    <th colspan="2">
+                        <small>Uploaded by: <a href="{{ route('browse.user',['id' => $uploaderId]) }}">{{ $uploader }}</a> ({{ $createdAt }})</small>
+                    </th>
+                </tr>
+
+                <tr>
+                    <td>Song: {{ $songName }} - {{ $songSubName }}</td>
+                    <td class="has-text-right">Version: {{$version}}</td>
+                </tr>
+
+                <tr>
+                    <td>Author: {{ $authorName }}</td>
+                    <td>Difficulties: {{ $difficulties }}</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Downloads: {{ $downloadCount }} || Finished: {{ $playedCount }}
+                    </td>
+                    <td>
+                        Lighting Events: {{ $events }}
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">{!! nl2br(e($description)) !!}</td>
+                </tr>
+                
+                <tr>
+                    <td>
+                        @auth
+                            <div>
+                                <form action="{{ route('votes.submit',['key' => $key]) }}" method="post">
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="button" name="type" value="up">
+                                        <span class="icon"><i class="fas fa-thumbs-up"></i></span>
+                                        <span>Up {{ $upVotes }}</span>
+                                    </button>
+                                    <button type="submit" class="button" name="type" value="down">
+                                        <span class="icon"><i class="fas fa-thumbs-down"></i></span>
+                                        <span>Down {{ $downVotes }}</span>
+                                    </button>
+                                </form>
+                            </div>
+                            <br/>
+                        @endauth
+                    </td>
+                    <td class="text-right">
+                        @auth
+                            @if(auth()->id() == $uploaderId)
+                                <a class="button is-link" href="{{ route('browse.detail.edit',['id' => $id]) }}" role="button">Edit</a>
+                                <a class="button is-danger" href="{{ route('browse.detail.delete',['id' => $id]) }}" role="button">Delete</a>
+                            @endif
+                        @endauth
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
--- resources/views/themes/default/components/song-preview.blade.php
+++ resources/views/themes/default/components/song-preview.blade.php
@@ -1,38 +1,50 @@
-<a href="{{ $linkUrl }}"><h2>{{ $name }}</h2></a>
-<table id="song-{{ $id }}" class="table" style="table-layout:fixed;">
-    <tr>
-        <th rowspan="5" style="width: 15%;" class="text-center">
-            <div>
-                <img src="{{ $coverUrl }}" alt="{{ $name }}" style="min-width: 10em; max-width: 10em;">
-            </div>
-            <br/>
-            <div>
-                <a class="btn btn-default btn-primary" href="{{ $linkUrl }}" role="button">Details</a>
-            </div>
-        </th>
-        <th colspan="2">
-            <small>Uploaded by: <a href="{{ route('browse.user',['id' => $uploaderId]) }}">{{ $uploader }}</a> ({{ $createdAt }})</small>
-        </th>
-    </tr>
-    <tr>
-        <td>Song: {{ $songName }} - {{ $songSubName }}</td>
-        <td class="text-right">Version: {{$version}}</td>
-    </tr>
-    <tr>
-        <td>Author: {{ $authorName }}</td>
-        <td>Difficulties: {{ $difficulties }}</td>
-    </tr>
-    <tr>
-        <td>
-            Downloads: {{ $downloadCount }} || Finished: {{ $playedCount }} || <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> {{ $upVotes }} / <span
-                    class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> {{ $downVotes }}
-        </td>
-        <td>Lighting Events: {{ $events }}</td>
-    </tr>
-    <tr>
-        <td colspan="2">
-            <a class="btn btn-default" href="{{ $downloadUrl }}" role="button">Download File</a>
-            <a class="btn btn-default" href="https://bsaber.com/songs/{{ $id }}" role="button">view at bsaber.com</a>
-        </td>
-    </tr>
-</table>
+<div style="margin-bottom: 15px;">
+    <h2 style="border-bottom: 1px solid #dbdbdb;margin-bottom: 10px;">
+        <a href="{{ $linkUrl }}" class="has-text-weight-semibold is-size-3">{{ $name }}</a>
+    </h2>
+
+    <div class="columns is-mobile">
+        <div class="column" style="max-width: 200px;">
+            <img src="{{ $coverUrl }}" alt="{{ $name }}" class="image" style="border-radius: 8px;margin-bottom: 8px;">
+
+            <a class="button is-link has-text-weight-bold is-fullwidth" href="{{ $linkUrl }}" role="button">Details</a>
+        </div>
+
+        <div class="column">
+            <table class="table is-fullwidth">
+                <tr>
+                    <th colspan="2">
+                        <small>Uploaded by: <a href="{{ route('browse.user',['id' => $uploaderId]) }}">{{ $uploader }}</a> ({{ $createdAt }})</small>
+                    </th>
+                </tr>
+
+                <tr>
+                    <td>Song: {{ $songName }} - {{ $songSubName }}</td>
+                    <td class="has-text-right">Version: {{$version}}</td>
+                </tr>
+
+                <tr>
+                    <td>Author: {{ $authorName }}</td>
+                    <td>Difficulties: {{ $difficulties }}</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Downloads: {{ $downloadCount }} || Finished: {{ $playedCount }} || <i class="fas fa-thumbs-up"></i> {{ $upVotes }} / <i
+                            class="fas fa-thumbs-down"></i> {{ $downVotes }}
+                    </td>
+                    <td>
+                        Lighting Events: {{ $events }}
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <a class="button" href="{{ $downloadUrl }}" role="button">Download File</a>
+                        <a class="button" href="https://bsaber.com/songs/{{ $id }}" role="button">View on BeastSaber</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>

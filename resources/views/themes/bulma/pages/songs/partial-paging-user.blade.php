78d8367f0eaecda64c2db9bef9188c5bfb038fec
Byorun <byorun@cyber-cortex.net>
2018-10-15 12:47:17 +0200

    fixed next/previous button in bulma theme

--- resources/views/themes/bulma/pages/songs/partial-paging-user.blade.php
+++ resources/views/themes/bulma/pages/songs/partial-paging-user.blade.php
@@ -1,15 +1,18 @@
-@if($start >= 0 && $songs && count($songs) == $steps )
-    <a href="{{ route(Route::currentRouteName(),['start' => $start + $steps,'id' => $userId]) }}" class="pull-right">
-        <button type="button" class="btn btn-default">
-            Next Page ({{(floor($start / $steps)?: 1) +1 }}) <span class="glyphicon glyphicon-forward" aria-hidden="true"></span>
-        </button>
-    </a>
-@endif
+<div class="has-text-centered">
+    @if($start >= 0 && ($start - $steps) >= 0)
+        <a href="{{ route(Route::currentRouteName(),['start' => $start - $steps,'id' => $userId]) }}" class="pull-left">
+            <button type="button" class="button is-info has-text-weight-bold">
+                <span class="glyphicon glyphicon-backwards" aria-hidden="true"></span> Previous Page ({{ (floor($start / $steps) ?: 1) }})
+            </button>
+        </a>
+    @endif
 
-@if($start >= 0 && ($start - $steps) >= 0)
-    <a href="{{ route(Route::currentRouteName(),['start' => $start - $steps,'id' => $userId]) }}" class="pull-left">
-        <button type="button" class="btn btn-default">
-            <span class="glyphicon glyphicon-backwards" aria-hidden="true"></span> Previous Page ({{ (floor($start / $steps) ?: 1) }})
-        </button>
-    </a>
-@endif
+    @if($start >= 0 && $songs && count($songs) == $steps )
+        <a href="{{ route(Route::currentRouteName(),['start' => $start + $steps,'id' => $userId]) }}"
+           class="pull-right">
+            <button type="button" class="button is-info has-text-weight-bold">
+                Next Page ({{(floor($start / $steps)?: 1) +1 }}) <span class="glyphicon glyphicon-forward" aria-hidden="true"></span>
+            </button>
+        </a>
+    @endif
+</div>

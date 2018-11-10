37b173f67f4e92fba93d6151379e99842949a9c5
Roderik <rastahoff@gmail.com>
2018-11-10 12:42:39 +0100

    Changed viewport behavior

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=1024">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('og-meta')
     <title>Beat Saver @yield('title')</title>

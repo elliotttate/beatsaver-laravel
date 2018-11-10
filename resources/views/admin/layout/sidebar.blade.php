bdb54c08acf6f8dd8445d4129aede797bb5dfbb2
Roderik <rastahoff@gmail.com>
2018-11-10 16:30:50 +0100

    Modified sidebar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,19 +1,7 @@
 <section class="sidebar">
     <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="treeview">
-            <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dropdown</span>
-                <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Dropdown item 1</a></li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Dropdown item 2</a></li>
-            </ul>
-        </li>
-        <li><a href="#"><i class="fa fa-user"></i> <span>Users</span></a></li>
+        <li class="header">Application Entities</li>
+        <li><a href="{{ route('admin.users.index') }}"><i class="fa fa-user"></i> <span>Users</span></a></li>
         <li><a href="#"><i class="fa fa-music"></i> <span>Songs</span></a></li>
     </ul>
 </section>
